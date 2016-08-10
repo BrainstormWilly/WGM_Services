0dea9c9d8edd9fcd5b7aca153ea9a1c8b7d8a123
Bill Langley <brainstormwilly@gmail.com>
2016-08-10 16:23:46 -0700

    Vin65 Session Policy fix

--- vin65/includes/session_policy.php
+++ vin65/includes/session_policy.php
@@ -1,7 +1,15 @@
 <?php
 
   session_start();
-  if( !isset($_SESSION['username']) && !isset($_SESSION['password']) ){
+
+  if( !isset($_SESSION['username']) || !isset($_SESSION['password']) || !isset($_SESSION['service']) ){
+    unset($_SESSION);
+    session_destroy();
+    header("Location: " . $_ENV['V65_HOST'] . "/index.php");
+    exit();
+  }elseif ($_SESSION['service'] != 'Vin65') {
+    unset($_SESSION);
+    session_destroy();
     header("Location: " . $_ENV['V65_HOST'] . "/index.php");
     exit();
   }
