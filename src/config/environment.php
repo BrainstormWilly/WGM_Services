e9d6d2e6fae41ac50b7ffc374f60478025b5798c
Bill Langley <brainstormwilly@gmail.com>
2016-11-16 17:47:00 -0800

    testing...

--- src/config/environment.php
+++ src/config/environment.php
@@ -32,15 +32,15 @@ if( $_ENV['APP_ENV'] === 'dev' ){
   $_ENV['APP_INCLUDES'] = $_ENV['SERVICES_ROOT'] . "/includes";
   $_ENV['TYPES_ROOT'] = $_ENV['SERVICES_ROOT'] . "/types";
   $_ENV['UPLOADS_PATH'] = "./uploads/";
-  $_ENV['APP_HOST'] = "/";
+  $_ENV['APP_HOST'] = "/wgm";
 
-  $_ENV['BLOYAL_HOST'] = "/bloyal";
+  $_ENV['BLOYAL_HOST'] = $_ENV['APP_HOST'] . "/bloyal";
   $_ENV['BLOYAL_INCLUDES'] = $_ENV['SERVICES_ROOT'] . "/bloyal/includes";
 
-  $_ENV['NEX_HOST'] = "/nexternal";
+  $_ENV['NEX_HOST'] = $_ENV['APP_HOST'] . "/nexternal";
   $_ENV['NEX_INCLUDES'] =  $_ENV['SERVICES_ROOT'] . "/nexternal/includes";
 
-  $_ENV['V65_HOST'] = "/vin65";
+  $_ENV['V65_HOST'] = $_ENV['APP_HOST'] . "/vin65";
   $_ENV['V65_INCLUDES'] = $_ENV['SERVICES_ROOT'] . "/vin65/includes";
 }else{
   $_ENV['SERVICES_ROOT'] = dirname(dirname(__DIR__));
