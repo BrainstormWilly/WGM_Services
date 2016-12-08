bec7667dd7b8ceffcad3d4d9059c033f3cdabd7f
Bill Langley <brainstormwilly@gmail.com>
2016-12-08 14:21:34 -0800

    in progress

--- src/config/environment.php
+++ src/config/environment.php
@@ -12,10 +12,16 @@ if( $_ENV['APP_ENV'] === 'dev' ){
   $_ENV['SERVICES_ROOT'] = dirname(dirname(__DIR__));
 
   $_ENV['APP_ROOT'] = $_ENV['SERVICES_ROOT'] . "/src/app";
+  $_ENV['CONFIG_ROOT'] = $_ENV['SERVICES_ROOT'] . "/src/config";
   $_ENV['APP_INCLUDES'] = $_ENV['SERVICES_ROOT'] . "/includes";
   $_ENV['TYPES_ROOT'] = $_ENV['SERVICES_ROOT'] . "/types";
   $_ENV['UPLOADS_PATH'] = "./uploads/";
   $_ENV['APP_HOST'] = "/services";
+
+  $_ENV['DB_HOST'] = 'localhost';
+  $_ENV['DB_USERNAME'] = 'root';
+  $_ENV['DB_PASSWORD'] = 'ok4root';
+  $_ENV['DB_NAME'] = 'wgm';
 
   $_ENV['BLOYAL_HOST'] = "/services/bloyal";
   $_ENV['BLOYAL_INCLUDES'] = $_ENV['SERVICES_ROOT'] . "/bloyal/includes";
@@ -28,7 +34,13 @@ if( $_ENV['APP_ENV'] === 'dev' ){
 }else if($_ENV['APP_ENV'] === 'syn'){
   $_ENV['SERVICES_ROOT'] = dirname(dirname(__DIR__));
 
+  $_ENV['DB_HOST'] = 'localhost';
+  $_ENV['DB_USERNAME'] = 'root';
+  $_ENV['DB_PASSWORD'] = '';
+  $_ENV['DB_NAME'] = 'wgm';
+
   $_ENV['APP_ROOT'] = $_ENV['SERVICES_ROOT'] . "/src/app";
+  $_ENV['CONFIG_ROOT'] = $_ENV['SERVICES_ROOT'] . "/src/config";
   $_ENV['APP_INCLUDES'] = $_ENV['SERVICES_ROOT'] . "/includes";
   $_ENV['TYPES_ROOT'] = $_ENV['SERVICES_ROOT'] . "/types";
   $_ENV['UPLOADS_PATH'] = "./uploads/";
@@ -45,19 +57,25 @@ if( $_ENV['APP_ENV'] === 'dev' ){
 }else{
   $_ENV['SERVICES_ROOT'] = dirname(dirname(__DIR__));
 
+  $_ENV['DB_HOST'] = 'localhost';
+  $_ENV['DB_USERNAME'] = 'root';
+  $_ENV['DB_PASSWORD'] = '';
+  $_ENV['DB_NAME'] = 'wgm';
+
   $_ENV['APP_ROOT'] = $_ENV['SERVICES_ROOT'] . "/src/app";
+  $_ENV['CONFIG_ROOT'] = $_ENV['SERVICES_ROOT'] . "/src/config";
   $_ENV['APP_INCLUDES'] = $_ENV['SERVICES_ROOT'] . "/includes";
   $_ENV['TYPES_ROOT'] = $_ENV['SERVICES_ROOT'] . "/types";
   $_ENV['UPLOADS_PATH'] = "./uploads/";
-  $_ENV['APP_HOST'] = "/services";
+  $_ENV['APP_HOST'] = "/";
 
-  $_ENV['BLOYAL_HOST'] = "/services/bloyal";
+  $_ENV['BLOYAL_HOST'] = "/bloyal";
   $_ENV['BLOYAL_INCLUDES'] = $_ENV['SERVICES_ROOT'] . "/bloyal/includes";
 
-  $_ENV['NEX_HOST'] = "/services/nexternal";
+  $_ENV['NEX_HOST'] = "/nexternal";
   $_ENV['NEX_INCLUDES'] =  $_ENV['SERVICES_ROOT'] . "/nexternal/includes";
 
-  $_ENV['V65_HOST'] = "/services/vin65";
+  $_ENV['V65_HOST'] = "/vin65";
   $_ENV['V65_INCLUDES'] = $_ENV['SERVICES_ROOT'] . "/vin65/includes";
 
 }
