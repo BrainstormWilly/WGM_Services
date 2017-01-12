468f277de6c2654df2efce707d2555c363982110
Bill Langley <brainstormwilly@gmail.com>
2017-01-12 13:54:09 -0800

    removed insertshipping reference

--- vin65/list.php
+++ vin65/list.php
@@ -59,8 +59,7 @@
                 <a class="list-group-item" href='types/contacts/add_contact_type_to_contact.php'>AddContactTypeToContact<span class="glyphicon glyphicon-chevron-right pull-right"></span></a>
                 <a class="list-group-item" href='types/contacts/add_shipping_address.php'>AddShippingAddress<span class="glyphicon glyphicon-chevron-right pull-right"></span></a>
                 <a class="list-group-item" href='types/contacts/get_contact.php'>GetContact<span class="glyphicon glyphicon-chevron-right pull-right"></span></a>
-                <a class="list-group-item" href='types/contacts/get_shipping_address.php'>GetShippingAddress<span class="glyphicon glyphicon-chevron-right pull-right"></span></a>
-                <a class="list-group-item" href='types/contacts/insert_shipping_address.php'>InsertShippingAddress<span class="glyphicon glyphicon-chevron-right pull-right"></span></a>    
+                <a class="list-group-item" href='types/contacts/get_shipping_address.php'>GetShippingAddress<span class="glyphicon glyphicon-chevron-right pull-right"></span></a>  
                 <a class="list-group-item" href='types/contacts/search_contact_types.php'>SearchContactTypes<span class="glyphicon glyphicon-chevron-right pull-right"></span></a>
                 <a class="list-group-item" href='types/contacts/upsert_contact.php'>UpsertContact<span class="glyphicon glyphicon-chevron-right pull-right"></span></a>
                 <a class="list-group-item" href='types/contacts/update_shipping_address.php'>UpdateShippingAddress<span class="glyphicon glyphicon-chevron-right pull-right"></span></a>
